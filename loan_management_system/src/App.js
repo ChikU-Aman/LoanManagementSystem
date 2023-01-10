3a494a0321c15923db3da6d1bb5529cf9da92e5c
Anuj Singh Solanki <anuj.solanki@Yash.com>
2023-01-10 18:45:58 +0530

    index.js and app.js

--- loan_management_system/src/App.js
+++ loan_management_system/src/App.js
@@ -9,7 +9,7 @@ import Login from "./cibil/Login";
 import ForgotPassword from './cibil/ForgotPassword';
 import UpdateProfile from './cibil/UpdateProfile';
 import Base from './cibil/Home';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import ContactForm from './components/ContactForm';
 
 const App = () => {
@@ -41,50 +41,29 @@ const App = () => {
 
   return (
     <div>
-      <ContactForm/>
-      <form>
-      <input
-          type="text"
-          id="first"
-          name="first"
-          value={first}
-          onChange={event => setFirst(event.target.value)}
-          autoComplete="off"
-        />
-        <input
-          type="text"
-          id="last"
-          name="last"
-          value={last}
-          onChange={event => setLast(event.target.value)}
-          autoComplete="off"
-        />
-
-        <button>Submit</button>
-      </form>
+    {/* // <div className="App">
+    //   <CibilCheck />
+    // </div> */}
+      {/* <Base>
+        <Container
+          className="d-flex align-items-center justify-content-center"
+          style={{ minHeight: "100vh" }}>
+          <div className="w-100" style={{ maxWidth: "400px" }}>
+            <Router>
+              <AuthProvider>
+                <Routes>
+                  <Route exact path='/' element={<Dashboard />} />
+                  <Route path="/signup" element={<Signup />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/forgot-password" element={<ForgotPassword />} />
+                  <Route path="/update-profile" element={<UpdateProfile />} />
+                </Routes>
+              </AuthProvider>
+            </Router>
+          </div>
+        </Container>
+      </Base> */}
     </div>
-    // <div className="App">
-    //   <CibilCheck/>
-    // </div>
-    <Base>
-      <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}>
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Router>
-            <AuthProvider>
-              <Routes>
-                <Route exact path='/' element={<Dashboard />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/update-profile" element={<UpdateProfile />} />
-              </Routes>
-            </AuthProvider>
-          </Router>
-        </div>
-      </Container>
-    </Base>
   );
 };
 
--- loan_management_system/src/index.js
+++ loan_management_system/src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Header from './components/Header';
 import ContactForm from './components/ContactForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,23 +11,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'react-circular-progressbar/dist/styles.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="col-md-6">
-          <Header />
-          <ContactForm />
-        </div>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header/>
+    <ContactForm/>
     <App />
   </React.StrictMode>
 );
