8da8ee3be892b18705ebee2886e6c638a7d04b24
Roshini.s <roshini.s@yash.com>
2023-01-10 16:47:38 +0530

    done with enquiry form

--- loan_management_system/src/App.js
+++ loan_management_system/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+import ContactForm from './components/ContactForm';
 
-function App() {
+const App = () => {
+  const [first, setFirst] = useState('');
+  const [last, setLast] = useState('');
+
+  const handleSubmit = () => {
+    console.log('form submitted ✅');
+  };
+
+  useEffect(() => {
+    const keyDownHandler = event => {
+      console.log('User pressed: ', event.key);
+
+      if (event.key === 'Enter') {
+        event.preventDefault();
+
+        //  call submit function here
+        handleSubmit();
+      }
+    };
+
+    document.addEventListener('keydown', keyDownHandler);
+
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ContactForm/>
+      <form>
+      <input
+          type="text"
+          id="first"
+          name="first"
+          value={first}
+          onChange={event => setFirst(event.target.value)}
+          autoComplete="off"
+        />
+        <input
+          type="text"
+          id="last"
+          name="last"
+          value={last}
+          onChange={event => setLast(event.target.value)}
+          autoComplete="off"
+        />
+
+        <button>Submit</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
